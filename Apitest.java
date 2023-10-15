2d67809f20c9c4fbc69445a01b2d7c8323482282
Heorhii Sliusarenko <gosha01072005@gmail.com>
2023-10-15 17:43:08 +0200

    cursworke

---
+++ Apitest.java
@@ -0,0 +1,67 @@
+ public class Rewestst {
+  package api;
+} import io.restassurd.http.ContentType;
+import org.junit.Test;
+import static io.restassurud.ResstAssred.given;
+public class Apitest ReqresTest{
+} private final static  String URL =https://json2csharp.com/code-converters/json-to-pojo
+@Test
+ public void checkAvatarAndIdTest(){
+} List<UserData> users = given()
+         .when()
+         .contentType(Content.Type.JSON)
+         .get(s:URL+"/api/users!page=2")
+         .then().log ().all()
+         .extract().body().jsonPath().getList(protected "data, "( UeserData){
+
+         }
+
+ @Override
+ public int describeContents(){
+         return 0;
+         }
+
+ @Override
+ public void writeToParcel(@android.support.annotation.NonNull android.os.Parcel dest,int flags){
+
+         }
+
+ public static final android.os.Parcelable.Creator<"data"> CREATOR=new Parcelable.Creator<"data">(){
+ @Override
+ public "data" createFromParcel(Parcel in){
+         return new "data"(in);
+         }
+
+ @Override
+ public "data"[]newArray(int size){
+         return new "data"[size];
+         }
+         };)
+         {
+
+         }
+
+ @Override
+ public int describeContents(){
+         return 0;
+         }
+
+ @Override
+ public void writeToParcel(@android.support.annotation.NonNull android.os.Parcel dest,int flags){
+
+         }
+
+ public static final android.os.Parcelable.Creator<Rewestst> CREATOR=new Parcelable.Creator<Rewestst>(){
+ @Override
+ public Rewestst createFromParcel(Parcel in){
+         return new Rewestst(in);
+         }
+
+ @Override
+ public Rewestst[]newArray(int size){
+         return new Rewestst[size];
+         }
+         };)
+
+
+
