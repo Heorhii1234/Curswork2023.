74fc5d57997cd0fab5e6f54ac5384db8a4d6c3ea
Heorhii1234 <135061327+Heorhii1234@users.noreply.github.com>
2023-10-15 18:17:16 +0200

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,22 @@
+public class Student {
+} private final int id;
+  private final String firstName;
+  private final String lastName;
+
+  public Student(int id, String firstName,lastName){
+      this.id = id;
+  }    this.fristName = firstName;
+       this.lastName = lastName;{
+
+        }
+        public int getId(){
+           return id;
+
+        }
+    public String getFirstName(){
+           return firstNsme{
+        }
+        public String getLastName(){
+               return lastName;
+        }
+        }
